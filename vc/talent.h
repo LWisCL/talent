3e1a7e73317bbd8a0d35cbbf885c71c81dd6b957
磊陈 <1210024038@qq.com>
2018-12-28 13:42:45 +0800

    xxx

---
+++ vc/talent.h
@@ -0,0 +1,16 @@
+//
+//  talent.h
+//  Pods
+//
+//  Created by 陈磊 on 2018/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface talent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ vc/talent.m
@@ -0,0 +1,12 @@
+//
+//  talent.m
+//  Pods
+//
+//  Created by 陈磊 on 2018/12/28.
+//
+
+#import "talent.h"
+
+@implementation talent
+
+@end
